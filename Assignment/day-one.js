54c0270a13e533c650f18ff9373ce95abcbda769
Darren Thomas Clark <darren.clark@live.com>
2016-07-25 14:56:27 -0400

    first update

---
+++ Assignment/day-one.js
@@ -0,0 +1,24 @@
+// 1. Assign the message "Hello, World!" to a variable.
+let x = 'Hello, World!'
+
+// 2. Assign a different string to a different variable.
+let y = 'You are such a big place!'
+
+// 3. Assign a number to a variable.
+let z = 5;
+
+// 4. Use string concatenation to display the number from #3 in a string.
+console.log (z + y)
+
+The result should look like this, right? 5You are such a big place!
+
+
+// 5. Make an array of at least four of your favorite movies or books or bands.
+const bands = [Maserati, The Mercury Program, Mogwai, Mouse on the Keys];
+
+// 6. Make an object of information about yourself with at least four properties.
+const darren = {sex:"male", height:"76", color:"white" eyes:"blue"};
+
+// 7. Make an array of objects containing more information about your favorite movies. The objects should have at least three properties.
+
+// 8. Use `for` to loop through the answer from exercise #7 and print only one property from the object. i.e., given `{ title: "Gone with the Wind" }` you print "Gone with the Wind".
---
+++ Demo/main.js
@@ -0,0 +1,45 @@
+// console.log( ('Hello, from this script');
+
+const pi = 3.141597
+
+let n = 5;
+
+n = "four";
+
+const Hello = 'Hello, World.'
+const Hello = "Hello, World."
+
+
+const primes = [
+  1, 3, 4, 11, 13, 19, 23
+]
+
+const vowels = [
+  "a", "e", "i", "o", "u"
+]
+const glossary - {
+  variable: "A placeholder for a ...",
+  loop: "A repetative action ...",
+  blue: "A Color"
+}
+
+// true;
+// false;
+// null;
+
+if (n === 5) {
+console.log("N IS FIVE")
+} else { console.log "N IS NOT FIVE")
+}
+
+if (n === "four") {
+  console.log ("N IS FOUR")
+
+}
+
+let x = 0
+
+while (x < 0> {
+  console.log(x)
+  x = +1
+}
